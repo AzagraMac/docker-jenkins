15d9128dab48ec5ea9a8b389c61eea5e4b6cdf85
Jose l. Azagra <azagramac@gmail.com>
2019-03-13 17:22:53 +0100

    update file

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 var http = require('http');
-
-var server = http.createServer(function (request, response) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write('<!DOCTYPE html>'+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write('<!DOCTYPE html>'+
 '<html>'+
 ' <head>'+
 ' <meta charset="utf-8" />'+
-' <title>Hello World!!!</title>'+
-' </head>'+
+' <title>Hello World</title>'+
+' </head>'+ 
 ' <body>'+
-' <p>copyright 2019 <strong>AzagraMac</strong>!</p>'+
+' <p>Copyright 2019 <strong>AzagraMac</strong>!</p>'+
 ' </body>'+
 '</html>');
-        response.end();
+	res.end();
 });
 
-server.listen(8000);
+server.listen(8080);
 console.log("Server listening at http://127.0.0.1:8000/");
